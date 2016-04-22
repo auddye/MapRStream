0590b4a6703eea2d3b3fd88b241bc4f21d4afa85
root <root@v5.poc.com>
2016-04-21 20:10:46 -0400

    add SampleConsumer_CG1.java

---
+++ src/main/java/openkb/stream/SampleConsumer_CG1.java
@@ -0,0 +1,63 @@
+package openkb.stream;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.TopicPartition;
+
+import java.io.IOException;
+import java.util.*;
+
+public class SampleConsumer_CG1 {
+    // Set the stream and topic to read from.
+    public static String topic = "/stream/s1:info";
+
+    // Declare a new consumer.
+    public static KafkaConsumer consumer;
+
+    public static void main(String[] args) throws IOException {
+        configureConsumer(args);
+
+        // Subscribe to the topic.
+        List<String> topics = new ArrayList<>();
+        topics.add(topic);
+        consumer.subscribe(topics);
+
+        // Set the timeout interval for requests for unread messages.
+        boolean stop = false;
+        int pollTimeout = 10000;
+        while (!stop) {
+            // Request unread messages from the topic.
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(pollTimeout);
+            Iterator<ConsumerRecord<String, String>> iterator = consumerRecords.iterator();
+            if (iterator.hasNext()) {
+                while (iterator.hasNext()) {
+                    ConsumerRecord<String, String> record = iterator.next();
+                    // Iterate through returned records, extract the value
+                    // of each message, and print the value to standard output.
+                    System.out.println((" Consumed Record: " + record.toString()));
+                }
+            } else {
+                stop = true;
+            }
+        }
+        consumer.close();
+        System.out.println("All done.");
+    }
+
+    /* Set the value for a configuration parameter.
+       This configuration parameter specifies which class
+       to use to deserialize the value of each message.*/
+    public static void configureConsumer(String[] args) {
+        Properties props = new Properties();
+        props.put("key.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("auto.offset.reset",
+                "latest");
+        props.put("group.id",
+                "CG1");
+        consumer = new KafkaConsumer<String, String>(props);
+    }
+}
+
